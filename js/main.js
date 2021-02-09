ca01cadc8e5096bff26356b16febfe7695e98f88
Ilya Homyakov <36329586+IlyaHomyakov@users.noreply.github.com>
2021-02-09 21:51:23 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// function giveRes() {
-// }
-// giveRes();
-//1536
-console.log(screen.orientation.type);
-// if(screen.orientation.type != "landscape-primary"){
-//     alert("Похоже вы просматриваете сайт с телефона, версия для телефонов будет доступна позже")
-// }
+function scroll() {
+    var
+}
